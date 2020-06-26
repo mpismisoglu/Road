0965d5d6e5068cc0004985e57576fa7f38c63469
MertPismisoglu <mpismisoglu@gmail.com>
2020-06-27 00:05:23 +0300

    initial

--- Road Adventure/GameScene.swift
+++ Road Adventure/GameScene.swift
@@ -11,79 +11,76 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var bg = SKSpriteNode()
+    var bg2 = SKSpriteNode()
+    var bg3 = SKSpriteNode()
+
+    var parallax = SKAction()
     
+   
+    
+  
     override func didMove(to view: SKView) {
+       bg = SKSpriteNode(imageNamed: "road")
+        bg.position = CGPoint(x: self.frame.width/2, y:0)
+       bg.zPosition = 3
+        bg.size = CGSize(width: self.frame.width, height: self.frame.height*1.1)
+
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+       bg2 = SKSpriteNode(imageNamed: "road")
+        bg2.position = CGPoint(x: self.frame.size.width/2, y:self.frame.height)
+       bg2.zPosition = 3
+        bg2.size = CGSize(width: self.frame.size.width, height: self.frame.height*1.1)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        
+       bg3 = SKSpriteNode(imageNamed: "road")
+        bg3.position = CGPoint(x: self.frame.size.width/2, y:self.frame.height + bg2.position.y)
+       bg3.zPosition = 3
+        bg3.size = CGSize(width: self.frame.size.width, height: self.frame.height*1.1)
+
+       self.addChild(bg)
+       self.addChild(bg2)
+       self.addChild(bg3)
+        parallax = SKAction.repeatForever(SKAction.move(by: CGVector(dx:0, dy: -self.frame.size.height - 50), duration: 2))
+
+        bg.run(parallax)
+        bg2.run(parallax)
+        bg3.run(parallax)
+        
+        
     }
     
     
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
+   
+   
+   
     
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
+    
+  
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+               
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
+   
     
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        
+        if bg.position.y <= -self.frame.size.height {
+               bg.position.y = self.frame.size.height * 2
+               
+           }
+        if bg2.position.y <= -self.frame.size.height {
+               bg2.position.y = self.frame.size.height * 2
+              
+           }
+        if bg3.position.y <= -self.frame.size.height {
+               bg3.position.y = self.frame.size.height * 2
+            
+           }
     }
 }
